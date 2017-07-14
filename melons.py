f5b7d83a4cacce3d758be01f099c61917987a278
Hackbright Student <no-reply@hackbrightacademy.com>
2017-07-13 18:02:08 -0700

    Fixed errors, complete through part 3

--- melons.py
+++ melons.py
@@ -39,10 +39,12 @@ class DomesticMelonOrder(AbstractMelonOrder):
     def __init__(self, species, qty):
         """Initialize melon order attributes."""
 
+        tax = 0.08
+
         super(DomesticMelonOrder, self).__init__(species,
                                                  qty,
                                                  "domestic",
-                                                 0.08)
+                                                 tax)
 
 
 class InternationalMelonOrder(AbstractMelonOrder):
@@ -51,13 +53,35 @@ class InternationalMelonOrder(AbstractMelonOrder):
     def __init__(self, species, qty, country_code):
         """Initialize melon order attributes."""
 
+        tax = 0.17
+
         super(InternationalMelonOrder, self).__init__(species,
                                                       qty,
                                                       'international',
-                                                      0.17)
+                                                      tax)
         self.country_code = country_code
 
     def get_country_code(self):
         """Return the country code."""
 
         return self.country_code
+
+
+class GovernmentMelonOrder(AbstractMelonOrder):
+    """A melon order for the US government."""
+
+    def __init__(self, species, qty):
+        """Initialize melon order attributes."""
+        tax = 0.0
+
+        super(GovernmentMelonOrder, self).__init__(species,
+                                                   qty,
+                                                   'government',
+                                                   tax)
+
+        self.passed_inspection = False
+
+    def mark_inspection(self, passed):
+        """Record the fact than an order has passed inspection."""
+
+        self.passed_inspection = passed
